02776f92f0f6723dbcbb6f5d6f77883e88b5d846
jdmedlock <jdmedlock@gmail.com>
2017-04-15 08:42:06 -0500

    corrected form input in SearchTerm.js

--- src/components/client/SearchTerm.js
+++ src/components/client/SearchTerm.js
@@ -1,9 +1,23 @@
 import React, { Component } from 'react';
 
 class SearchTerm extends Component {
+
+  constructor( props ) { 
+    super( props ); 
+    this.state = { 
+      searchTerms: null
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+ }
+
+  handleChange(event) {
+    this.setState({searchTerms: event.target.value});
+  }
+
   handleSubmit(event) {
+    console.log(`${this.state.searchTerms}`);
     event.preventDefault();
-    this.props.onSubmit(this.textInput.value);
   }
 
   render() {
@@ -13,10 +27,9 @@ class SearchTerm extends Component {
           <br/>
           <label>Search Term(s):</label>
           &nbsp; &nbsp;
-          <input name="searchTerms" id="search-terms" required
-            type="text" 
+          <input name="searchTerms" id="search-terms" required type="text" 
+            value={this.state.value} onChange={this.handleChange}
             placeholder="Enter term(s) separated by commas"
-            ref={(input) => { this.textInput = input; }} 
           />
           &nbsp;&nbsp;&nbsp;
           <input type="submit" value="Submit"/>
