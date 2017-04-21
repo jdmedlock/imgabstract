ceb28462fa8489091c7b17a13bd2a41a06e974e9
jdmedlock <jdmedlock@gmail.com>
2017-04-20 22:00:41 -0500

    Changed test command in package.json to reference /tests directory

---
+++ tests/helpers.js
@@ -0,0 +1,15 @@
+require('babel-core/register')({
+    ignore: false
+});
+
+import { expect } from 'chai';
+import { sinon, spy } from 'sinon';
+import { mount, render, shallow } from 'enzyme';
+
+global.expect = expect;
+global.sinon = sinon;
+global.spy = spy;
+
+global.mount = mount;
+global.render = render;
+global.shallow = shallow;
